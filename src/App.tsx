30a8e352919bc7ba6bd5b5988b3386d823a882f6
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-07 10:31:16 -0400

    somewhat of a popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            
+            <Popup trigger={<button className="button">Trigger</button>} position="center center">
+                <div><h1>Welcome to website!!</h1></div>
+            </Popup>
         </div>
     );
 }
