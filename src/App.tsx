86521c1f4c76b627231229ad0d06d53d2bac4490
maybeep <60990725+maybeep@users.noreply.github.com>
2021-10-07 13:53:33 -0400

    random css updates and manual merge with maybeep branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React,{useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 function App(): JSX.Element {
-    let [count,SetCount]=useState(0)
+    const [count, SetCount] = useState(0);
     return (
         <div className="App">
+            <div className="popup">
+                <Popup trigger={<button className="button">Welcome</button>} position="center center">
+                    <div><h1>Welcome to website!!</h1></div>
+                </Popup>
+            </div>
             Counter: {count}
             <button onClick={()=>SetCount(count+1)}>Click me!</button>
             <button onClick={()=>SetCount(count-1)}>Minus me!</button>
